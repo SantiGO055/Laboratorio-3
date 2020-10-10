5717317d7891cdae4310d9fd20e4371cdf76167f
SantiGO055 <santigonzalez05@gmail.com>
2020-10-10 20:08:39 -0300

    .

--- practicaPrimerParcial/js/funciones.js
+++ practicaPrimerParcial/js/funciones.js
@@ -21,7 +21,7 @@ function validar(){
     var botonModificar = document.getElementById("botonModificar");
     // var mostrarDiv = document.getElementById("botonMostrarDiv");
     var esconderDiv = document.getElementById("btnEsconder");
-    // var eliminarCelda = document.getElementById("eliminarCelda");
+    var botonEliminar = document.getElementById("botonEliminar");
 
     // var tabla = document.getElementById("tabla"),rIndex,cIndex;
     
@@ -33,8 +33,9 @@ function validar(){
     nombre.addEventListener("blur",validarNombre);
     apellido.addEventListener("blur",validarApellido);
     fecha.addEventListener("blur",validarFecha);
+    
     //sexo.addEventListener("blur",validarSexo);
-    botonModificar.addEventListener("click",validarBoton);
+    //botonModificar.addEventListener("click",validarBoton);
 
     leerPersonaGet();
     
@@ -49,46 +50,71 @@ function cerrarDiv(){
 //#region validaciones de campos
 
 function validarNombre(){
-
-    if (this.value.length < 3) {
-        document.getElementById("nombre").className = document.getElementById("nombre").className = " inputError";
-        return 0;
+    nombreCheck = document.getElementById("nombre");
+    if (nombreCheck.value != "") {
+        if (nombreCheck.value.length < 3) {
+            document.getElementById("nombre").className = document.getElementById("nombre").className = " inputError";
+            return false;
+        }
+        else{
+            document.getElementById("nombre").className = document.getElementById("nombre").className.replace(" inputError", "inputSinError");
+            
+            return true;
+        } 
     }
     else{
-        document.getElementById("nombre").className = document.getElementById("nombre").className.replace(" inputError", "inputSinError");
-        return 1;
+        alert ("Ingrese nombre");
+        return false;
     }
+    
 }
 
 function validarApellido(){
-
-    if (this.value.length < 3) {
-        document.getElementById("apellido").className = document.getElementById("apellido").className = " inputError";
-        return 0;
+    apellidoCheck = document.getElementById("apellido");
+    
+    if (apellidoCheck.value != "") {
+        if (apellidoCheck.value.length < 3 || apellidoCheck.value === "") {
+            document.getElementById("apellido").className = document.getElementById("apellido").className = " inputError";
+            return false;
+        }
+        else{
+            document.getElementById("apellido").className = document.getElementById("apellido").className.replace(" inputError", "inputSinError");
+            
+            return true;
+        }
     }
     else{
-        document.getElementById("apellido").className = document.getElementById("apellido").className.replace(" inputError", "inputSinError");
-        return 1;
+        alert ("Ingrese apellido");
+        return false;
     }
 }
 function validarFecha(){
-    var retorno = false;
+    
     var f = new Date();
     /**Año, mes, dia = aaaa-mm-dd ej. 2020-12-5 */
     fechaHoy =f.getFullYear()+ "-" + (f.getMonth() +1)+ "-" + + f.getDate();
     // fechaHoy = f.getDate() + "-" + (f.getMonth() +1) + "-" + f.getFullYear();
     var fechaAModif = $("fecha");
     // console.log(fechaAModif);
-    if (!fechaAModif) {
+    if (!fechaAModif || fechaAModif >= fechaHoy) {
         document.getElementById("fecha").className = document.getElementById("fecha").className = " inputError";
-    }
-    // console.log(fechaHoy);
-    if (fechaHoy >= fechaAModif) {
-        return retorno;
+        
+        return false;
     }
     else{
-        return retorno;
+        document.getElementById("fecha").className = document.getElementById("fecha").className.replace(" inputError", "inputSinError");
+        
+        return true;
     }
+    // console.log(fechaHoy);
+    // if (fechaHoy >= fechaAModif) {
+    //     retorno = true;
+    //     return retorno;
+    // }
+    // else{
+    //     retorno = false;
+    //     return retorno;
+    // }
     
 }
 function validarSexo(){
@@ -99,24 +125,29 @@ function validarSexo(){
     }
     else{
         document.getElementById("sexo").className = document.getElementById("sexo").className = " inputError";
+        return false;
     }
 }
 //#endregion
 
 
 function validarBoton(){
-    if (nombre.value === "" || apellido.value === "" || fecha.value === "dd/mm/aaaa") {
-        alert("Ingrese los campos correctos");
+    // if (validarNombre() && validarApellido() && validarFecha() && validarSexo()) {
+    //     return true;
+    // }
+    // else{
+    //     alert("Ingrese campos correctos");
+    //     return false;
+    // }
+    if (validarNombre() && validarApellido() && validarFecha()) {
+        return true;
     }
-    //tcuerpo.innerHTML += "<tr><td>"+nombre.value+"</td><td>"+apellido.value+"</td><td><a href='#' onclick='eliminarCelda(this)'>Eliminar</a></td></tr>";
-    
+    else{
+        alert("Ingrese los campos correctos");
+        return false;
+    }
 
 }
-    
-// function eliminarCelda(celda){
-//     var d = celda.parentNode.parentNode.rowIndex;
-//     document.getElementById('tabla').deleteRow(d);
-// }
 
 
 function leerPersonaGet(){
@@ -129,33 +160,34 @@ function agregarDatos(id,nombre,apellido,fecha,sexo){
     var row = document.createElement("tr");
 
     row.addEventListener("dblclick",modificarDatos);
+    row.setAttribute("id",id);
 
     var colNombre = document.createElement("td");
     // colNombre.addEventListener("dblclick",modificarDatos);
     var textNombre = document.createTextNode(nombre);
     colNombre.appendChild(textNombre);
-    colNombre.setAttribute("id",id);
+    // colNombre.setAttribute("id",id);
     row.appendChild(colNombre);
 
     var colApellido = document.createElement("td");
     var textApe = document.createTextNode(apellido);
     // colApellido.addEventListener("dblclick",modificarDatos);
     colApellido.appendChild(textApe);
-    colApellido.setAttribute("id",id);
+    // colApellido.setAttribute("id",id);
     row.appendChild(colApellido);
 
     var colFecha = document.createElement("td");
     var textFecha = document.createTextNode(fecha);
     // colFecha.addEventListener("dblclick",modificarDatos);
     colFecha.appendChild(textFecha);
-    colFecha.setAttribute("id",id);
+    // colFecha.setAttribute("id",id);
     row.appendChild(colFecha);
 
     var colSexo = document.createElement("td");
     var textSexo = document.createTextNode(sexo);
     // colSexo.addEventListener("dblclick",modificarDatos);
     colSexo.appendChild(textSexo);
-    colSexo.setAttribute("id",id);
+    // colSexo.setAttribute("id",id);
     row.appendChild(colSexo);
     
     
@@ -196,15 +228,7 @@ function respuestaGet(){
     
 }
 
-function eliminarCelda(event){
-    event.preventDefault();
-    // console.log(event.target.parentNode.parentNode);
 
-    var fila = event.target.parentNode.parentNode;
-    var tabla = document.getElementById("tcuerpo");
-    tabla.removeChild(fila);
-    //TODO enviar al servidor por post la persona eliminada
-}
 
 function MostrarDivModificar(){
     document.getElementById("div").hidden = false;
@@ -237,13 +261,14 @@ function modificarDatos(event){
 
     event.preventDefault();
     var fila = event.target.parentNode.childNodes;
+    var sacoID = event.target.parentNode;
     
     
     var nombre = fila[0].textContent;
     var apellido = fila[1].textContent;
     var fecha = fila[2].textContent;
     var sexo = fila[3].textContent;
-    var id = fila[3].getAttribute("id");
+    var id = sacoID.getAttribute("id");
     
 
     // personaAux.push(nombre,apellido,fecha,sexo,id);
@@ -258,60 +283,143 @@ function modificarDatos(event){
     
     // document.getElementById("sexo").value = sexo;
     
+
     botonModificar.addEventListener("click",function(){
         
         nombreAux = $("nombre");
         apellidoAux = $("apellido");
         fechaAux = $("fecha");
         sexoAux = obtenerValoresRadioButton();
-
-        ejecutarPostModificar(id,nombreAux,apellidoAux,fechaAux,sexoAux)
+        if (validarBoton()) {
+            ejecutarPostModificar(id,nombreAux,apellidoAux,fechaAux,sexoAux);
+        }
+    });
+    botonEliminar.addEventListener("click",function(){
         
-        
-        
-        
-        // fila[0].textContent = nombreAux;
-        // fila[1].textContent = apellidoAux;
-        // fila[2].textContent = fechaAux;
-        // fila[3].textContent = sexoAux;
-        //validarFecha();
-        
+        if (validarBoton()) {
+            ejecutarPostEliminar(id);
+        }
     });
     
 }
 
-function modificarCelda(event){
-    event.preventDefault();
-    var fila = event.target.parentNode.childNodes;
-    
-    console.log(fila);
+
+function modificarCelda(id,nombre,apellido,fecha,sexo){
 
 
-    fila[0].textContent = $("nombre");
-    fila[1].textContent = $("apellido");
-    fila[2].textContent = $("fecha");;
-    fila[3].textContent = obtenerValoresRadioButton();
+    /**obtengo el id de la celda */
+
+    var tcuerpo = document.getElementById("tcuerpo");
+    /**obtengo la lista de personas en el html */
+    var listaPersonas = tcuerpo.parentNode.childNodes[3].childNodes;
+    /**recorro la lista */
+    for (let index = 1; index < listaPersonas.length; index++) {
+        /**obtengo el atributo id de ese tr */
+        // console.log(listaPersonas[index].getAttribute("id"));
+        /**si coincide con el que quiero modificar, seteo los datos */
+        if(listaPersonas[index].getAttribute("id") === id){
+            listaPersonas[index].childNodes[0].textContent = nombre;
+            listaPersonas[index].childNodes[1].textContent = apellido;
+            listaPersonas[index].childNodes[2].textContent = fecha;
+            listaPersonas[index].childNodes[3].textContent = sexo;
+        }
+
+    }
     
+    // event.preventDefault();
+    // var fila = event.target.parentNode.childNodes;
+    
+    
+
+
+    // fila[0].textContent = $("nombre");
+    // fila[1].textContent = $("apellido");
+    // fila[2].textContent = $("fecha");;
+    // fila[3].textContent = obtenerValoresRadioButton();
+    
+}
+function eliminarCelda(id){
+    
+    
+    var tabla = document.getElementById("tcuerpo");
+    /**obtengo la lista de personas en el html */
+    var listaPersonas = tabla.parentNode.childNodes[3].childNodes;
+    // console.log(listaPersonas);
+    /**recorro la lista */
+    for (let index = 1; index < listaPersonas.length; index++) {
+        /**obtengo el atributo id de ese tr */
+        // console.log(listaPersonas[index]);
+        /**si coincide con el que quiero modificar, seteo los datos */
+       
+        if(listaPersonas[index].getAttribute("id") === id){
+            // console.log(listaPersonas[index].getAttribute("id"));
+            // console.log(id);
+            var fila = listaPersonas[index];
+            tabla.removeChild(fila);
+            return true;
+        }
+        //TODO borrar el id del tr que levante de la lista
+        // console.log(fila);
+        
+        // tabla.removeChild(fila);
+        // listaPersonas[index].childNodes[0].removeChild(id);
+        
+
+    }
+    
+}
+function obtenerIDForm(){
+    document.getElementById("")
 }
 function ejecutarPostModificar(id,nombre,apellido,fecha,sexo){
     let objetoPersona = {"id" : id, "nombre" : nombre, "apellido" : apellido, "fecha" : fecha, "sexo" : sexo };
                         
     var stringJson = JSON.stringify(objetoPersona);
-    
     var sendPost = stringJson;
-    
-    // var sendPost = "nombre="+nombre+"&apellido="+apellido+"&fecha="+fecha+"&telefono="+telefono;
     ajax("POST","http://localhost:3000/editar",respuestaPostModificar,sendPost)
 
-    //modificarCelda();
+    document.getElementById("div").hidden = true;
+}
+function ejecutarPostEliminar(id){
+    let objetoPersona = {"id" : id};
+                        
+    var stringJson = JSON.stringify(objetoPersona);
+    var sendPost = stringJson;
+    // ajax("POST","http://localhost:3000/eliminar",respuestaPostEliminar,sendPost);
+    ajax("POST","http://localhost:3000/eliminar",function(){
+        respuestaPostEliminar(id);
+    },sendPost);
+
     document.getElementById("div").hidden = true;
     
-    
+}
+function respuestaPostEliminar(id){
+    document.getElementById("loaderImage").hidden = false;
+    if(peticionHttp.readyState == 4){
+        if (peticionHttp.status == 200) {
+            /**cuando tengo respuesta de servidor lo casteo a obj json */
+            var respuesta  = peticionHttp.responseText;
+            var respuestaArray = JSON.parse(respuesta);
+            if (respuestaArray.type != 'error') {
+
+                document.getElementById("loaderImage").hidden = true;
+                console.log(id);
+                eliminarCelda(id);
+            }
+            else{
+                console.log(respuestaArray);
+            }
+
+        }
+        else{
+            alert("error");
+            
+        }
+    }
 }
 
-
 function respuestaPostModificar(){
-    
+    document.getElementById("loaderImage").hidden = false;
     if(peticionHttp.readyState == 4){
         if (peticionHttp.status == 200) {
             /**cuando tengo respuesta de servidor lo casteo a obj json */
@@ -319,38 +427,11 @@ function respuestaPostModificar(){
             var respuestaArray = JSON.parse(respuesta);
             //console.log(respuestaArray);
             if (respuestaArray.type != 'error') {
-                var nombreAux = $("nombre");
-                var apellidoAux = $("apellido");
-                var fechaAux = $("fecha");
-                var sexoAux = obtenerValoresRadioButton();
+
+                document.getElementById("loaderImage").hidden = true;
                 
-                // console.log(e.target);
-                //TODO modificar celda
-
-                /**datos que responde el servidor */
-                console.log(respuestaArray.id);
-
-
                 /**tengo que modificar la celda que le hice doble click con los datos que me devolvio */
-                console.log(tcuerpo.length);
-                for (let index = 0; index < tcuerpo.length; index++) {
-                    const element = array[index];
-                    
-                }
-                
-                //TODO detengo el spinner
-                
-                // console.log(nombreAux);
-                // console.log(apellidoAux);
-                // console.log(fechaAux);
-                // console.log(sexoAux);
-
-                
-
-                /** como se que el servidor me responde ok, tomo los valores que ingrese de los campos y los agrego a la lista */
-                //modificarCelda(nombreAux,apellidoAux,fechaAux,sexoAux);
-
-                //tcuerpo.innerHTML += "<tr><td>"+nombreAux+"</td><td>"+apellidoAux+"</td><td>"+fechaAux+"</td><td>"+telefonoAux+"</td><td>"+"<a href='#' id='eliminarCelda' value='Eliminar' onclick=eliminarCelda(this)>Eliminar</a>"+"</td></tr>";
+                modificarCelda(respuestaArray.id,respuestaArray.nombre,respuestaArray.apellido,respuestaArray.fecha,respuestaArray.sexo);
             }
             else{
                 //console.log(respuestaArray.type);
